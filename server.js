b0720e2a15e52c1773df9825cadd9654efc3d624
DeepthiLatha <kathuladeepthilatha10@gmail.com>
2024-12-25 18:52:22 +0530

    Modified .env

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const customerRoutes = require("./routes/customerRoutes");
-require("dotenv").config();
 const app = express();
-const PORT = 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGO_URI, {
+const mongoURI = process.env.MONGO_URI;
+if (!mongoURI) {
+  console.error("Error: MONGO_URI is not defined in the environment variables.");
+  process.exit(1); // Exit the application if MONGO_URI is missing
+}
+
+mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
@@ -26,6 +31,7 @@ app.use((err, req, res, next) => {
   res.status(500).send({ message: "Something went wrong!" });
 });
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
